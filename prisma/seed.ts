911c0e2aa83dacd51c9ed671ea7df0193c02b4cf
yrnana <nyryn0945@gmail.com>
2021-12-05 18:53:56 +0900

    fix: 스키마 수정

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,54 +1,47 @@
 import { Event, EventDate, Place, PrismaClient } from '@prisma/client';
 
-const placeSeeds: Omit<Place, 'id'>[] = [
+const placeSeeds: Place[] = [
   {
+    id: 8106109,
     name: '코엑스 D홀',
     lat: 37.51235237999761,
     lng: 127.05901053033,
-    placeId: 8106109,
   },
   {
+    id: 8360965,
     name: 'SETEC',
     lat: 37.495683550002,
     lng: 127.071966780003,
-    placeId: 8360965,
   },
 ];
 
-const eventSeeds: (Omit<Event, 'id' | 'placeId'> & { placeId: number })[] = [
+const eventSeeds: Omit<Event, 'id'>[] = [
   {
-    placeId: 8106109,
     name: '2022 케이캣페어 서울',
+    placeId: 8106109,
     placeDetail: null,
     image:
       'https://s3-ap-northeast-2.amazonaws.com/esang-kcat/wp-content/uploads/2021/11/23014247/22_1%EC%9B%94%EC%BD%94%EC%97%91%EC%8A%A4%EC%BA%A3_01.png',
     homepage: 'https://k-cat.co.kr/2022cat_coex/',
   },
   {
-    placeId: 8360965,
     name: '제19회 궁디팡팡 캣페스타',
+    placeId: 8360965,
     placeDetail: null,
     image: 'https://www.gdppcat.com/images/last/19_poster.jpg',
     homepage: 'https://www.gdppcat.com/',
   },
 ];
 
-const db = new PrismaClient();
+const prisma = new PrismaClient();
 
 async function seed() {
-  const places = await Promise.all(
-    placeSeeds.map((data) => db.place.create({ data })),
-  );
+  await Promise.all(placeSeeds.map((data) => prisma.place.create({ data })));
+
   const events = await Promise.all(
-    eventSeeds.map((data) =>
-      db.event.create({
-        data: {
-          ...data,
-          placeId: places.find((place) => data.placeId === place.placeId).id,
-        },
-      }),
-    ),
+    eventSeeds.map((data) => prisma.event.create({ data })),
   );
+
   const dateSeeds: EventDate[] = [];
   events.forEach((event) => {
     if (event.name === '2022 케이캣페어 서울') {
@@ -95,7 +88,7 @@ async function seed() {
       );
     }
   });
-  await Promise.all(dateSeeds.map((data) => db.eventDate.create({ data })));
+  await Promise.all(dateSeeds.map((data) => prisma.eventDate.create({ data })));
 }
 
 seed();
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -7,12 +7,47 @@ export type GetEventResponse = Event & {
 
 export type GetEventsResponse = GetEventResponse[];
 
+export type PostUploadsResponse = {
+  files: Express.Multer.File[];
+};
+
 export type PostEventsRequestBody = {
-  events: Omit<Prisma.EventCreateInput, 'dates' | 'place'>;
+  event: Omit<Prisma.EventCreateInput, 'dates' | 'place'>;
   dates: Prisma.EventDateCreateWithoutEventInput[];
   place: Omit<Prisma.PlaceUncheckedCreateInput, 'event'>;
 };
 
 export type ErrorResponse = {
   error: boolean | string;
+};
+
+export type KakaoPlaceMeta = {
+  totalCount: number;
+  pageableCount: number;
+  isEnd: boolean;
+  sameName: {
+    region: string[];
+    keyword: string;
+    selectedRegion: string;
+  };
+};
+
+export type KakaoPlaceDocument = {
+  id: string;
+  placeName: string;
+  categoryName: string;
+  categoryGroupCode: string;
+  categoryGroupName: string;
+  phone: string;
+  addressName: string;
+  roadAddressName: string;
+  x: string;
+  y: string;
+  placeUrl: string;
+  distance: string;
+};
+
+export type KakaoPlace = {
+  meta: KakaoPlaceMeta;
+  documents: KakaoPlaceDocument[];
 };
